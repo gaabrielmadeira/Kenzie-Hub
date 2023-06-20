eb97cfd4da52878bd5a319ac5c1a464c6d1ed992
Gabriel Madeira <gabscastello95@gmailcom>
2023-06-19 23:01:18 -0300

    entrega realizada

--- src/styles/inputs.js
+++ src/styles/inputs.js
@@ -8,18 +8,31 @@ const StyledPlaceholder = css`
 
   color: var(--color-grey-1);
 `
+const StyledInputText = css`
+  font-family: var(--font-primary);
+  font-weight: var(--font-weight-3);
+  font-size: var(--font-size-1);
+  line-height: 163%;
+
+  color: var(--color-grey-0);
+`
 
 export const StyledInput = styled.input`
   width: 100%;
   height: 40px;
 
-  ${StyledPlaceholder}
+  ${StyledInputText}
+
+  ::placeholder{
+    ${StyledPlaceholder}
+  }
 
   background: var(--color-grey-2);
   border: 1px solid var(--color-grey-2);
   border-radius: 3px;
   padding: 0.625rem 1rem;
   margin-top: 0.3125rem;
+
 
   :focus{
     border: 1px solid var(--color-grey-0);
